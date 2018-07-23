b97abab76be680e6d2cecc5856c14468a83434c7
Nicholas Powers <njpowers7915@gmail.com>
2018-07-23 14:35:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ const drivers = ["Milo", "Otis", "Garfield"];
 function destructivelyAppendDriver(name) {
   return drivers.push(name);
 }
+
+function destructivelyPrependDriver(name) {
+  return drivers.push(name);
+}
